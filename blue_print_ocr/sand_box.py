2d169147e5a70c4ea9421cb11271e3f52c2dcf82
William Davis <william.davis@missionsupport.us>
2024-04-16 08:15:06 -0600

    adding new sandbox file

---
+++ blue_print_ocr/sand_box.py
@@ -0,0 +1,5 @@
+import cv2 as cv
+import sys
+
+blueprint_url = "C:\\Users\\William.davis\\OneDrive - msiGCCH\\Pictures\\Screenshots\\Screenshot 2024-04-08 123224.png"
+img = cv.imread(cv.samples.findFile(blueprint_url))
