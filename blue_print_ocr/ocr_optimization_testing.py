eea0c49ba67de25a3dfdf9ef52bdf646ea03807d
William Davis <william.davis@missionsupport.us>
2024-04-24 13:59:24 -0600

    adding ROI and cropping sourch from tesseract project

--- blue_print_ocr/ocr_optimization_testing.py
+++ blue_print_ocr/ocr_optimization_testing.py
@@ -1,12 +1,14 @@
 import cv2 as cv
 import sys
 from matplotlib import pyplot as plt
-from opt_preprocessing import get_all_grayscales, detect_lines, show_merged_lines
+from opt_preprocessing import get_all_grayscales, detect_lines, show_merged_lines, crop_ROI, find_text
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
 
 blueprint_url =  "C:\\Users\\William.davis\\Desktop\\python_data_set\\static\\images\\table_test.png"
+#blueprint_url =  "C:\\Users\\William.davis\\Desktop\\python_data_set\\static\\images\\Screenshot 2024-04-24 081630.png"
+
 export_url = "C:\\Users\\William.davis\\OneDrive - msiGCCH\\Pictures\\Screenshots\\test_updated_image_cv2.png"
 
 
@@ -25,13 +27,24 @@ def ocr_magic(bluprint_url, export_url):
         # cv.waitKey(0)
         all_lines.append(image_lines)
 
-    for i in range(10):
-        plt.subplot(2,5,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
-        plt.title(titles[i])
-        plt.xticks([]),plt.yticks([])
+    # SHOWS ALL EXAMPLES OF cv.LINE FOR EACH DIFFEREING THRESHOLD
+
+    # for i in range(10):
+    #     plt.subplot(2,5,i+1),plt.imshow(images[i],'gray',vmin=0,vmax=255)
+    #     plt.title(titles[i])
+    #     plt.xticks([]),plt.yticks([])
 
     
-    show_merged_lines(all_lines, blueprint_url)
+    merged_horizontal_lines, merged_vertical_lines, cImage_color = show_merged_lines(all_lines, blueprint_url)
+
+    first_line_index = 1
+    last_line_index = len(merged_vertical_lines)
+
+    for i in range(first_line_index, last_line_index):
+
+    crop_ROI(cImage_color, merged_horizontal_lines, merged_vertical_lines,)
+
+
 
     # return plt.show()
 
--- blue_print_ocr/opt_preprocessing.py
+++ blue_print_ocr/opt_preprocessing.py
@@ -53,7 +53,7 @@ def overlapping_filter(lines, sorting_index):
         line_current = lines[i]
         if(i>0):
             line_previous = lines[i-1]
-            if( (line_current[sorting_index] - line_previous[sorting_index]) > 20):
+            if( (line_current[sorting_index] - line_previous[sorting_index]) > 4):
                 #filtered_lines.append(line_current)
                 pass
             else:
@@ -212,7 +212,7 @@ def line_check(line, merged_lines, buffer_zone):
 
 def show_merged_lines(all_lines, image_url):
     
-    merged_horizontal_lines, merged_vertical_lines = merge_lines(all_lines, 10)
+    merged_horizontal_lines, merged_vertical_lines = merge_lines(all_lines, 3)
 
 
 
@@ -234,4 +234,30 @@ def show_merged_lines(all_lines, image_url):
     cv.waitKey(0)
     cv.destroyAllWindows()
 
-  
+    return merged_horizontal_lines, merged_vertical_lines, cImage_color
+
+def get_cropped_image(image, x, y, w, h):
+    cropped_image = image[y:y+h, x:x+w]
+    return cropped_image
+
+def crop_ROI(image, horizontal, vertical, left_line_index, right_line_index, top_line_index, bottom_line_index, offset=4):
+    x1 = vertical[left_line_index][2] + offset
+    y1 = horizontal[top_line_index][3] + offset
+    x2 = vertical[right_line_index][2] - offset
+    y2 = horizontal[bottom_line_index][3] - offset
+
+    w = x2 - x1
+    h = y2 - y1
+
+    cropped_image = get_cropped_image(image, x1, y1, w, h)
+
+    return cropped_image, (x1, y1, w, h)
+
+def find_text(cropped_frame, is_number = False):
+    if(is_number):
+        text = pytesseract.image_to_string(cropped_frame, config= '-c tessedit_char_whitelist=0123456789 --psm 11 --oem 3')
+
+    else: 
+        text = pytesseract.image_to_string(cropped_frame, config= '--psm 11 --oem 3')
+
+    return text
